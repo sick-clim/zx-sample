73a61e75887eccfaf55cdc33b6e7cdf3b9bd51c7
user.name sick-clim <46617682+sick-clim@users.noreply.github.com>
2024-03-15 15:58:16 +0900

    update

--- hello.mjs
+++ hello.mjs
@@ -1,5 +1,10 @@
-#!usr/bin/env zx
 
-const msg = "Hello World"
-await $`echo ${msg}`
+const status = await $`git status --porcelain`
+
+console.log(`stdoub: ${status.stdout}`)
+console.log(`stderr: ${status.stderr}`)
+console.log(`exitCode: ${status.exitCode}`)
+console.log(`length: ${status.lentgth}`)
+console.log(`status: ${status}`)
+console.log(status)
 
